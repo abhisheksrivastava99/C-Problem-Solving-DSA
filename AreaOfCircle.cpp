42ba0757f5914db7b6ded856459b4634522b0907
Abhishek Srivastava <abhisheksrivastavacapncook@gmail.com>
2021-06-13 17:09:56 +0530

    Add files via upload

---
+++ AreaOfCircle.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<cmath>
+#include<math.h>
+
+using namespace std;
+
+int main()
+{
+    float radius,area;
+    cin>>radius;
+    area=3.14*radius*radius;
+    cout<<"Area of circle is "<<" "<<area<<endl;
+    return 0;
+}
