c83a4e8c26ec0fbf12232004376f5ec9d0de4437
Abhishek Srivastava <abhisheksrivastavacapncook@gmail.com>
2021-06-13 18:06:54 +0530

    Create YoungorOld.cpp

---
+++ YoungorOld.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age;
+    cout << "Enter age" <<endl;
+    cin>>age;
+    if(age>=12 && age<=50)
+    {cout<<"Young"<<endl;}
+
+    else
+    {cout<<"Old"<<endl;}
+    return 0;
+}
