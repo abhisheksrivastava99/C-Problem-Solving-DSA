a7b0ec23a1c930c3bb8d7791316b3cc7952004e0
Abhishek Srivastava <abhisheksrivastavacapncook@gmail.com>
2021-06-14 19:55:19 +0530

    Create SumofNaturalNumbers.cpp

---
+++ SumofNaturalNumbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int num;
+int sum=0;
+cout << "Enter value of num: ";
+cin >> num;
+for(int i=1;i<=num;i++)
+{
+sum=sum+i;
+}
+cout<<"Sum is "<<sum<<endl;
+retur
